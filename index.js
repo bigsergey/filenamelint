2e15f83d2482e6469694b5e57d2df733eb476e4a
Sergey Bolshov <bolszow@gmail.com>
2019-11-05 21:35:01 +0100

    Validate filename to pass kebab-case notation

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const glob = require('fast-glob');
+const path =require('path');
 
 const join = (...args) => args.join('/');
 
+const kebabCase = /^[a-z0-9]*(?:[a-z0-9+-])+[a-z0-9]+$/;
+
 glob(join('**', '*'), { ignore: ['node_modules'] })
-  .then(console.log)
+  .then(files => {
+    files.forEach(file => {
+      const extensionName = path.extname(file);
+      const basename = path.basename(file, extensionName);
+
+      if (!kebabCase.test(basename)) {
+        console.error(`${file} has wrong name`);
+      }
+    });
+  })
   .catch(console.error);
