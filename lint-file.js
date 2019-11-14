451bcabbe4b88df71b0e2a0809ca01a0f783a087
Sergey Bolshov <bolszow@gmail.com>
2019-11-14 21:36:32 +0100

    validate-filename Use kebab case package instead of regexp

--- lint-file.js
+++ lint-file.js
@@ -1,11 +1,10 @@
 const path =require('path');
-
-const kebabCase = /^[a-z0-9]*(?:[a-z0-9+-])+[a-z0-9]+$/;
+const kebabCase = require('kebab-case');
 
 module.exports = function lintFile(file) {
   const extensionName = path.extname(file);
   const basename = path.basename(file, extensionName);
-  const isValid = kebabCase.test(basename);
+  const isValid = kebabCase(basename) === basename;
   const error = !isValid
     ? { message: `${file} has wrong name.` }
     : undefined;
