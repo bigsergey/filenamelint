caf9b7377a552fe942ff03472bdbf0a259de793d
Sergey Bolshov <bolszow@gmail.com>
2019-11-14 21:33:18 +0100

    validate-filename Use pipe instead of promise then chain

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const glob = require('fast-glob');
+const pipe = require('function-pipe');
 
 const lintFile = require('./lint-file');
 const logErrors = require('./log-errors');
 
 glob('**/*', { ignore: ['node_modules'] })
-  .then(files => files.map(lintFile))
-  .then(files => files.filter(({ isValid }) => !isValid))
-  .then(files => {
-    if (files.length > 0) {
-      logErrors(files);
-      process.exit(1);
-    } else {
-      console.log('Success!!')
-      process.exit(0);
+  .then(pipe(
+    files => files.map(lintFile),
+    files => files.filter(({ isValid }) => !isValid),
+    files => {
+      if (files.length > 0) {
+        logErrors(files);
+        process.exit(1);
+      } else {
+        console.log('Success!!')
+        process.exit(0);
+      }
     }
-  })
-  .catch(console.error);
+  ))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
