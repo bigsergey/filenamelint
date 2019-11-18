ae0af94892637fd71aebb4448b34d196e5c66658
bigsergey <bolszow@gmail.com>
2019-11-18 21:12:00 +0100

    Move main logic to separate file

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,3 @@
-import glob from 'fast-glob';
+import main from './main';
 
-import lintFiles from './lint-files';
-
-glob('**/*', { ignore: ['node_modules', 'README.md', 'CHANGELOG.md', 'LICENSE'] })
-  .then(files => {
-    const errorMessages = lintFiles(files);
-
-    if (errorMessages.length > 0) {
-      errorMessages.forEach(message => console.error(message));
-      process.exit(1);
-    } else {
-      console.log('Success!!');
-      process.exit(0);
-    }
-  })
-  .catch(error => {
-    console.error(error);
-    process.exit(1);
-  });
+main().then(process.exit);
---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import glob from 'fast-glob';
+
+import lintFiles from './lint-files';
+
+const ERROR_CODE = 1;
+const SUCCESS_CODE = 0;
+const ignore = ['node_modules', 'README.md', 'CHANGELOG.md', 'LICENSE'];
+
+export default function main(): Promise<number> {
+  return glob('**/*', { ignore })
+    .then(files => {
+      const errorMessages = lintFiles(files);
+
+      if (errorMessages.length > 0) {
+        errorMessages.forEach(message => console.error(message));
+        return Promise.resolve(ERROR_CODE);
+      } else {
+        return Promise.resolve(SUCCESS_CODE);
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      return Promise.resolve(ERROR_CODE);
+    });
+}
