cc9775bd5e202fe9f09dbb04657e4b294361fca9
bigsergey <bolszow@gmail.com>
2019-11-18 16:43:08 +0100

    Return empty string when filename is valid

--- src/lint-files.ts
+++ src/lint-files.ts
@@ -1,12 +1,12 @@
 import path from 'path';
 import kebabCase from 'kebab-case';
 
-function lintFile(file: string): string | undefined {
+function lintFile(file: string): string {
   const extensionName = path.extname(file);
   const basename = path.basename(file, extensionName);
   const isValid = kebabCase(basename) === basename;
 
-  return isValid ? undefined : `${file} has wrong name.`;
+  return isValid ? '' : `${file} has wrong name.`;
 }
 
 export default function lintFiles(files: string[]): string[] {
