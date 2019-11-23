3a4358fa172bd3c93e0d4068e61e862f8adf726e
bigsergey <bolszow@gmail.com>
2019-11-23 22:43:18 +0100

    Fix unit tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  testMatch: ['**/__tests__/**/*.+(ts|js)', '**/?(*.)+(spec|test).+(ts|js)'],
+  testMatch: ['**/?(*.)+(spec|test).+(ts|js)'],
   transform: {
     '^.+\\.(ts)?$': 'ts-jest',
   },
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts', '!src/**/__tests__/**/*.ts'],
 };
