4200c29cf2dd546fbe44d95a3b7282bd26b52c9c
bigsergey <bolszow@gmail.com>
2019-12-04 17:52:40 +0100

    Add support for other formats

--- src/lint-files.ts
+++ src/lint-files.ts
@@ -1,18 +1,20 @@
 import path from 'path';
 import kebabCase from 'kebab-case';
+import camelCase from 'camelcase';
+import { snakeCase } from 'snake-case';
 
 import { Formats } from './get-options';
 
-function getValidationFunction(format: Formats): KebabCaseFn {
+function validate(string: string, format: Formats): string {
   switch (format) {
     case Formats.kebabCase:
-      return kebabCase;
+      return kebabCase(string);
     case Formats.camelCase:
-      return kebabCase;
+      return camelCase(string);
     case Formats.pascalCase:
-      return kebabCase;
+      return camelCase(string, { pascalCase: true });
     case Formats.snakeCase:
-      return kebabCase;
+      return snakeCase(string);
     default:
       throw new Error(`"${format}" is unsupported format.`);
   }
@@ -21,8 +23,7 @@ function getValidationFunction(format: Formats): KebabCaseFn {
 function lintFile(file: string, format: Formats): string {
   const extensionName = path.extname(file);
   const basename = path.basename(file, extensionName);
-  const validationFunction = getValidationFunction(format);
-  const isValid = validationFunction(basename) === basename;
+  const isValid = validate(basename, format) === basename;
 
   return isValid ? '' : `${file} has wrong name.`;
 }
