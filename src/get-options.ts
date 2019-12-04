30ce913186ad5a96d330e25729c51d8d61083eb1
bigsergey <bolszow@gmail.com>
2019-12-04 17:31:45 +0100

    Add formats enum and use kebabCase as default

--- src/get-options.ts
+++ src/get-options.ts
@@ -1,13 +1,23 @@
+export enum Formats {
+  kebabCase = 'kebabCase',
+  camelCase = 'camelCase',
+  pascalCase = 'pascalCase',
+  snakeCase = 'snakeCase',
+}
+
 export interface Options {
-  ignore?: string[];
+  ignore: string[];
+  format: Formats;
 }
 
 export const defaultOptions = {
   ignore: ['node_modules/**', 'README.md', 'CHANGELOG.md', 'LICENSE'],
+  format: Formats.kebabCase,
 };
 
-export default function getOptions({ ignore }: Options = {}): Options {
+export default function getOptions({ ignore, format }: Partial<Options> = {}): Options {
   return {
     ignore: ignore || defaultOptions.ignore,
+    format: format || defaultOptions.format,
   };
 }
--- src/lint-files.ts
+++ src/lint-files.ts
@@ -1,17 +1,35 @@
 import path from 'path';
 import kebabCase from 'kebab-case';
 
-function lintFile(file: string): string {
+import { Formats } from './get-options';
+
+function getValidationFunction(format: Formats): KebabCaseFn {
+  switch (format) {
+    case Formats.kebabCase:
+      return kebabCase;
+    case Formats.camelCase:
+      return kebabCase;
+    case Formats.pascalCase:
+      return kebabCase;
+    case Formats.snakeCase:
+      return kebabCase;
+    default:
+      throw new Error(`"${format}" is unsupported format.`);
+  }
+}
+
+function lintFile(file: string, format: Formats): string {
   const extensionName = path.extname(file);
   const basename = path.basename(file, extensionName);
-  const isValid = kebabCase(basename) === basename;
+  const validationFunction = getValidationFunction(format);
+  const isValid = validationFunction(basename) === basename;
 
   return isValid ? '' : `${file} has wrong name.`;
 }
 
-export default function lintFiles(files: string[]): string[] {
+export default function lintFiles(files: string[], format: Formats): string[] {
   return files.flatMap(file => {
-    const lintResult = lintFile(file);
+    const lintResult = lintFile(file, format);
     return lintResult ? [lintResult] : [];
   });
 }
