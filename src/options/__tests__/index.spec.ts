47ec8bc0dcd1e66962c6e8fff237471efcca9e28
bigsergey <bolszow@gmail.com>
2020-02-06 16:06:24 +0100

    config-file Fix main getOptions function unit tests

--- src/options/__tests__/index.spec.ts
+++ src/options/__tests__/index.spec.ts
@@ -3,72 +3,72 @@ jest.mock('../get-options-from-file');
 import getOptions, { defaultOptions, Formats, Options } from '../index';
 import getOptionsFromFile from '../get-options-from-file';
 
-describe('getting options', () => {
-  const mockedGetOptionsFromFile = (getOptionsFromFile as unknown) as jest.Mock<Partial<Options>>;
+describe.only('getting options', () => {
+  const mockedGetOptionsFromFile = (getOptionsFromFile as unknown) as jest.Mock<Promise<Partial<Options>>>;
 
   afterEach(() => {
     mockedGetOptionsFromFile.mockRestore();
   });
 
   describe('default options', () => {
-    beforeEach(() => {
-      mockedGetOptionsFromFile.mockReturnValue({});
+    beforeEach(async () => {
+      await mockedGetOptionsFromFile.mockResolvedValue({});
     });
 
-    test('should return default options when custom options are empty', () => {
-      expect(getOptions()).toEqual(defaultOptions);
+    test('should return default options when custom options are empty', async () => {
+      expect(await getOptions()).toEqual(defaultOptions);
     });
 
-    test('should return default ignore option when all options are undefined', () => {
-      expect(getOptions({ ignore: undefined, format: undefined })).toEqual(defaultOptions);
+    test('should return default ignore option when all options are undefined', async () => {
+      expect(await getOptions({ ignore: undefined, format: undefined })).toEqual(defaultOptions);
     });
   });
 
   describe('non default config', () => {
     describe('custom options from cli', () => {
-      beforeEach(() => {
-        mockedGetOptionsFromFile.mockReturnValue({});
+      beforeEach(async () => {
+        await mockedGetOptionsFromFile.mockResolvedValue({});
       });
 
-      test('should rewrite default ignore option with custom one', () => {
+      test('should rewrite default ignore option with custom one', async () => {
         const ignore = ['custom', 'ignore'];
 
-        expect(getOptions({ ignore })).toHaveProperty('ignore', ignore);
+        expect(await getOptions({ ignore })).toHaveProperty('ignore', ignore);
       });
 
-      test('should rewrite default format option with custom one', () => {
+      test('should rewrite default format option with custom one', async () => {
         const format = Formats.snakeCase;
 
-        expect(getOptions({ format })).toHaveProperty('format', format);
+        expect(await getOptions({ format })).toHaveProperty('format', format);
       });
     });
 
     describe('custom options from config file', () => {
-      test('should rewrite default ignore option with custom one from config file', () => {
+      test('should rewrite default ignore option with custom one from config file', async () => {
         const ignore = ['ignore-from-config-file'];
-        mockedGetOptionsFromFile.mockReturnValue({ ignore });
+        await mockedGetOptionsFromFile.mockResolvedValue({ ignore });
 
-        expect(getOptions()).toHaveProperty('ignore', ignore);
+        expect(await getOptions()).toHaveProperty('ignore', ignore);
         expect(mockedGetOptionsFromFile).toHaveBeenCalledTimes(1);
       });
 
-      test('should rewrite default format option with custom one from config file', () => {
-        mockedGetOptionsFromFile.mockReturnValue({ format: Formats.snakeCase });
+      test('should rewrite default format option with custom one from config file', async () => {
+        await mockedGetOptionsFromFile.mockResolvedValue({ format: Formats.snakeCase });
 
-        expect(getOptions()).toHaveProperty('format', Formats.snakeCase);
+        expect(await getOptions()).toHaveProperty('format', Formats.snakeCase);
       });
 
-      test('should rewrite default and config file ignore option with custom one', () => {
+      test('should rewrite default and config file ignore option with custom one', async () => {
         const ignore = ['custom', 'ignore'];
-        mockedGetOptionsFromFile.mockReturnValue({ ignore: ['ignore-from-config-file'] });
+        await mockedGetOptionsFromFile.mockResolvedValue({ ignore: ['ignore-from-config-file'] });
 
-        expect(getOptions({ ignore })).toHaveProperty('ignore', ignore);
+        expect(await getOptions({ ignore })).toHaveProperty('ignore', ignore);
       });
 
-      test('should rewrite default and config file format option with custom one', () => {
-        mockedGetOptionsFromFile.mockReturnValue({ format: Formats.pascalCase });
+      test('should rewrite default and config file format option with custom one', async () => {
+        await mockedGetOptionsFromFile.mockResolvedValue({ format: Formats.pascalCase });
 
-        expect(getOptions({ format: Formats.snakeCase })).toHaveProperty('format', Formats.snakeCase);
+        expect(await getOptions({ format: Formats.snakeCase })).toHaveProperty('format', Formats.snakeCase);
       });
     });
   });
