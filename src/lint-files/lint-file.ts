4c950456f0d0844a22c5f48514fe6a2e3660b642
bigsergey <bolszow@gmail.com>
2020-03-19 22:22:04 +0100

    overrides Use chalk for better error messages

--- src/lint-files/lint-file.ts
+++ src/lint-files/lint-file.ts
@@ -1,4 +1,5 @@
 import path from 'path';
+import chalk from 'chalk';
 
 import { Formats } from '../options';
 import validate from './validate-string';
@@ -8,5 +9,5 @@ export default function lintFile(file: string, format: Formats): string | null {
   const basename = path.basename(file, extensionName);
   const isValid = validate(basename, format);
 
-  return isValid ? null : `${file} has wrong name ('${format}' is expected).`;
+  return isValid ? null : `${chalk.yellow(file)} has wrong name ('${format}' is expected).`;
 }
