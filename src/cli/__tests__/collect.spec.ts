d4482801c60dc131adbd2a11ef26ab30bb0db61c
bigsergey <bolszow@gmail.com>
2020-04-20 21:47:54 +0200

    update-commander Fix collect unit tests

--- src/cli/__tests__/collect.spec.ts
+++ src/cli/__tests__/collect.spec.ts
@@ -2,14 +2,14 @@ import collect from '../collect';
 
 describe('collect', () => {
   test('should create value when previous value is undefined', () => {
-    expect(collect(1)).toEqual([1]);
+    expect(collect('1')).toEqual(['1']);
   });
 
   test('should push value to empty the array', () => {
-    expect(collect(1, [])).toEqual([1]);
+    expect(collect('1', [])).toEqual(['1']);
   });
 
   test('should push value to end of the array', () => {
-    expect(collect(2, [1])).toEqual([1, 2]);
+    expect(collect('2', ['1'])).toEqual(['1', '2']);
   });
 });
